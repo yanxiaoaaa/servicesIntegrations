77a9016abd4f419328abe9d35f0cb7ab54decae7
yanxiao <790039090@qq.com>
2021-02-04 01:20:30 +0800

    增加发邮件功能

--- platformService/src/main/java/cn/bupt/service/impl/PlatformServiceImpl.java
+++ platformService/src/main/java/cn/bupt/service/impl/PlatformServiceImpl.java
@@ -8,12 +8,19 @@ import cn.bupt.service.PlatformService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.*;
 
 @Service("platformService")
 public class PlatformServiceImpl implements PlatformService {
     @Autowired
     private PlatformMapper platformMapper;
+
+    private static final String USER = "790039090@qq.com"; // 发件人称号，同邮箱地址
+    private static final String PASSWORD = "unrpadzwzxabbcej"; // 如果是qq邮箱可以使户端授权码，或者登录密码
+
 
     public List<Platform> listAllPlatform() {
         return platformMapper.listAllPlatform();
@@ -39,7 +46,61 @@ public class PlatformServiceImpl implements PlatformService {
         chat.setStatus(0);
         chat.setChatTime(new Date());
         platformMapper.saveChat(chat);
+        this.sendMail("2018140485@bupt.cn","您好，集成平台中有用户咨询您问题:"+content+"--------请及时回复","集成平台咨询提醒");
         return chat.getId();
+    }
+
+    /**
+     * @param to    收件人邮箱
+     * @param text  邮件正文
+     * @param title 标题
+     */
+    private Boolean sendMail(String to, String text, String title){
+        try {
+            final Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.host", "smtp.qq.com");
+
+            // 发件人的账号
+            props.put("mail.user", USER);
+            //发件人的密码
+            props.put("mail.password", PASSWORD);
+
+            // 构建授权信息，用于进行SMTP进行身份验证
+            Authenticator authenticator = new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    // 用户名、密码
+                    String userName = props.getProperty("mail.user");
+                    String password = props.getProperty("mail.password");
+                    return new PasswordAuthentication(userName, password);
+                }
+            };
+            // 使用环境属性和授权信息，创建邮件会话
+            Session mailSession = Session.getInstance(props, authenticator);
+            // 创建邮件消息
+            MimeMessage message = new MimeMessage(mailSession);
+            // 设置发件人
+            String username = props.getProperty("mail.user");
+            InternetAddress form = new InternetAddress(username);
+            message.setFrom(form);
+
+            // 设置收件人
+            InternetAddress toAddress = new InternetAddress(to);
+            message.setRecipient(Message.RecipientType.TO, toAddress);
+
+            // 设置邮件标题
+            message.setSubject(title);
+
+            // 设置邮件的内容体
+            message.setContent(text, "text/html;charset=UTF-8");
+            // 发送邮件
+            Transport.send(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public String read(Integer id) {
